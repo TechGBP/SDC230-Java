bf8cb392f4c79ea893e00a9063b0acbc6020c835
TechGBP <249761316+TechGBP@users.noreply.github.com>
2026-03-02 09:57:17 -0500

    Add files via upload

---
+++ example.java
@@ -0,0 +1,8 @@
+public class example {
+    private int age;  // private variable
+public void setAge(int newAge) {
+        if (newAge >= 0) {  // validation
+            age = newAge;
+        }
+    }
+}
